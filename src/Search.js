39e2c8d343a474b2c48fc109acbd2df46220c99b
jyang1003git config --global user.name <jerryyang1003@gmail.com>
2025-02-10 17:06:59 -0800

    changed Home.js to Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-
-function Home() {
+import { useEffect, useState } from "react"
+const search = () => {
     const getDogData = () => {
         fetch('https://frontend-take-home-service.fetch.com/dogs/search?Leonberg', {
             method: 'GET',
@@ -10,12 +9,5 @@ function Home() {
         .then(data => console.log('This is dog data:', data))
         .catch(err => console.error('Error fetching protected data:', err))
     }
-
-    return(
-        <div>
-            <input onClick={getDogData} type="text" placeholder="Search"></input>
-        </div>
-    )
 }
-
-export default Home;
+export default search
